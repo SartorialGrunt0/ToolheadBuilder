ccbcf870ffab2ba6640915ad8442b20239d28764
SartorialGrunt0 <sunnygalli@gmail.com>
2026-03-08 07:30:22 -0700

    Checkpoint to add UI stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwindcss from "@tailwindcss/vite";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://toolheadbuilder.com',
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'Toolhead Builder',
       social: [
         { icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }
       ],
+
       sidebar: [
         {
           label: 'Hotends',
